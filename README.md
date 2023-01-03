e40d7a83cab02aabf8520b63f3d4ac7894c8d6eb
Nicolas de Souza <nicolas.j.souza@gmail.com>
2023-01-03 14:36:05 -0300

    README

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Moises_bot
+# Bot Moisés
+
+Created time: January 3, 2023 2:12 PM
+Last edited time: January 3, 2023 2:31 PM
+Status: Caminhando
+Type: Post
+
+> Projeto desenvolvido como Trabalho de Conclusão de Curso (TCC) para o curso de Engenharia de Controle e Automação e apresentado em outubro de 2022
+> 
+
+## Overview do projeto Moisés
+
+```mermaid
+graph LR
+  A(APP)
+	B(API)
+	C(BOT)
+	D(ESP32)
+
+	A --> B
+	B --> A
+	C --> B
+	B --> C
+	C --> D
+
+	
+```
+
+## Objetivo
+
+<aside>
+✅ Criar uma interface amigável ao usuário que possibilite controlar a fechadura do sistema
+
+</aside>
+
+## Resultados e Discussões
+
+O bot interage de maneira simples com o usuário, não sendo necessário um grande conhecimento tecnológico ou maiores intimidades com a ferramenta, basta tão somente saber digitar.
+
+Como o objetivo aqui era construir um modelo razoavelmente maduro para simulações, o bot é bem simples e não possui grandes refinamentos, mas cumpre de maneira satisfatória o seu papel no sistema.
+
+![Bot: mensagens informativas](resource/Untitled.png)
+
+Bot: mensagens informativas
+
+![Bot: envio de mensagens](resource/Untitled%201.png)
+
+Bot: envio de mensagens
+
+## Implementações Futuras
+
+- Criação de arquivos de configuração
+- Adição de comandos do tipo markup comuns em outros bots Telegram
--- bot.py
+++ bot.py
@@ -2,18 +2,17 @@ import telebot
 import requests
 import json
 
-api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJOb21lIjoiTmljb2xhcyBKb3PDqSBkb3MgU2FudG9zIGRlIFNvdXphIiwiRW1haWwiOiJuaWNvbGFzLmouc291emFAZ21haWwuY29tIiwiTml2ZWxEZUFjZXNzbyI6ImFkbWluIn0.ik-Lhso2g97dbSfWfkSZebsPyOxX45wxPTN2TLQ9WeI"
-url = "http://192.168.1.4:7070/api/Reservas/fechadura/" + api_key
+api_key = "API_KEY_MOISES_API"
+url = "URL_API_MOISES" + api_key
 
-url_fechadura = "http://192.168.1.16/"
+url_fechadura = "ENDPOINT_ESP32"
 
-bot = telebot.TeleBot("5354549922:AAHKidv2uPGtYqYiombTM5tgYvQUYDTG8Ag")
+bot = telebot.TeleBot("")
 
 def get_acesso(senha_telegram):
     response = requests.get(url)
     response_json = json.loads(response.content)
-    # # {'tituloReserva': 'Cálculo 2', 'senhaReserva': '123', 'inicioReserva': '2022-07-21T09:32:00', 'fimReserva': '2022-07-21T15:30:00'}
-    # response_json['senhaReserva']
+
     if (response_json['senhaReserva'] == senha_telegram):
         requests.get(url_fechadura+'abrir')
         return True
@@ -42,118 +41,3 @@ def echo_all(message):
     
 
 bot.infinity_polling()
-
-# {
-#   'content_type': 'text',
-#   'id': 14, 
-#   'message_id': 14, 
-#   'from_user': 
-#       {
-#           'id': 95659176, 
-#           'is_bot': False, 
-#           'first_name': 'Nicolas', 
-#           'username': 'Nicolas_jsouza', 
-#           'last_name': 'De Souza', 
-#           'language_code': 'pt-br', 
-#           'can_join_groups': None, 
-#           'can_read_all_group_messages': None, 
-#           'supports_inline_queries': None, 
-#           'is_premium': None, 
-#           'added_to_attachment_menu': None
-#       }, 
-#   'date': 1657049142, 
-#   'chat': 
-#       {
-#           'id': 95659176, 
-#           'type': 'private', 'title': None, 
-#           'username': 'Nicolas_jsouza', 
-#           'first_name': 'Nicolas', 
-#           'last_name': 'De Souza', 
-#           'photo': None, 
-#           'bio': None, 
-#           'join_to_send_messages': None,
-#           'join_by_request': None,
-#           'has_private_forwards': None,
-#           'description': None,
-#           'invite_link': None,
-#           'pinned_message': None,
-#           'permissions': None,
-#           'slow_mode_delay': None,
-#           'message_auto_delete_time': None,
-#           'has_protected_content': None,
-#           'sticker_set_name': None,
-#           'can_set_sticker_set': None,
-#           'linked_chat_id': None,
-#           'location': None
-#       },
-#  'sender_chat': None,
-#  'forward_from': None,
-#  'forward_from_chat': None, 
-#  'forward_from_message_id': None,
-#  'forward_signature': None, 
-#  'forward_sender_name': None,
-#  'forward_date': None,
-#  'is_automatic_forward': None,
-#  'reply_to_message': None,
-#  'via_bot': None, 'edit_date': None,
-#  'has_protected_content': None,
-#  'media_group_id': None,
-#  'author_signature': None,
-#  'text': '/start',
-#  'entities': [<telebot.types.MessageEntity object at 0x7fcdf9f05180>],
-#  'caption_entities': None,
-#  'audio': None,
-#  'document': None,
-#  'photo': None, 
-#  'sticker': None,
-#  'video': None, 
-#  'video_note': None,
-#  'voice': None, 
-#  'caption': None,
-#  'contact': None,
-#  'location': None, 
-#  'venue': None,
-#  'animation': None,
-#  'dice': None,
-#  'new_chat_member': None,
-#  'new_chat_members': None,
-#  'left_chat_member': None,
-#  'new_chat_title': None, 
-#  'new_chat_photo': None,
-#  'delete_chat_photo': None, 
-#  'group_chat_created': None,
-#  'supergroup_chat_created': None,
-#  'channel_chat_created': None,
-#  'migrate_to_chat_id': None,
-#  'migrate_from_chat_id': None,
-#  'pinned_message': None,
-#  'invoice': None,
-#  'successful_payment': None,
-#  'connected_website': None, 
-#  'reply_markup': None, 
-#  'json': 
-#       {
-#           'message_id': 14,
-#           'from': 
-#               {
-#                   'id': 95659176,
-#                   'is_bot': False,
-#                   'first_name': 'Nicolas',
-#                   'last_name': 'De Souza',
-#                   'username': 'Nicolas_jsouza',
-#                   'language_code': 'pt-br'
-#               }, 
-#           'chat': 
-#               {
-#                   'id': 95659176,
-#                   'first_name': 'Nicolas',
-#                   'last_name': 'De Souza',
-#                   'username': 'Nicolas_jsouza',
-#                   'type': 'private'
-#                },
-#           'date': 1657049142,
-#           'text': '/start',
-#           'entities': [{'offset': 0, 'length': 6, 'type': 'bot_command'}]
-#       }
-# }
-
